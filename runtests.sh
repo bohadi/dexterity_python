941e645c8967c4f023ee4a3675c25137e805777f
bohadi <bohadi@protonmail.com>
2021-03-15 11:14:31 +0800

    diff integration test harness

--- runtests.sh
+++ runtests.sh
@@ -13,3 +13,21 @@ python3 -m unittest tests/unittest.py
 echo "▒▒▒▒▒▒▒▒▒▒▒▒▒▒ UNIT TESTS END ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒"
 
 
+cat tests/1.input | python main.py > tests/my/1.output
+cat tests/2.input | python main.py > tests/my/2.output
+cat tests/3.input | python main.py > tests/my/3.output
+cat tests/4.input | python main.py > tests/my/4.output
+cat tests/5.input | python main.py > tests/my/5.output
+cat tests/6.input | python main.py > tests/my/6.output
+
+echo ""
+echo ""
+
+echo "Diffing example inputs"
+
+diff -s tests/my/1.output tests/check/1.output
+diff -s tests/my/2.output tests/check/2.output
+diff -s tests/my/3.output tests/check/3.output
+diff -s tests/my/4.output tests/check/4.output
+diff -s tests/my/5.output tests/check/5.output
+diff -s tests/my/6.output tests/check/6.output
